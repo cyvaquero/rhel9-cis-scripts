03a10a350830cdeb27036381654b5585f30fc1c5
Guy Heckman <guy_heckman@aotx.uscourts.gov>
2023-08-10 08:53:41 -0400

    Renaming the auditd login/s auditd rule file.

--- 4-1-3-12_remediate.sh
+++ 4-1-3-12_remediate.sh
@@ -3,12 +3,12 @@
 # 4.1.3.12 Ensure login and logout events are collected
 # Filename: 4-1-3-12_remediate.sh
 
-[ -f /etc/audit/rules.d/logins.rules ] && rm /etc/audit/rules.d/logins.rules
+[ -f /etc/audit/rules.d/logins.rules ] && rm /etc/audit/rules.d/login.rules
 printf "
 ## 4.1.3.12 Ensure login and logout events are collected
 -w /var/log/lastlog -p wa -k logins
 -w /var/run/faillock -p wa -k logins
-" > /etc/audit/rules.d/50-login.rules
+" > /etc/audit/rules.d/50-logins.rules
 
 augenrules --load
 
